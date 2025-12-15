930f8021a3b7148cff1ea115f22787f883888875
Milana <m.milana2007.mil@gmail.com>
2025-12-15 21:36:23 +0300

    Add files via upload

--- cw10-12/models.py
+++ cw10-12/models.py
@@ -17,11 +17,15 @@ class Base(DeclarativeBase):
 class Product(Base):
     __tablename__ = "products" 
     name: Mapped[str] = mapped_column(String(255), nullable=False)
+    # description: Mapped[str] = mapped_column(String(255), nullable=False)
     quantity: Mapped[int] = mapped_column(nullable=False)
     price: Mapped[float] = mapped_column(nullable=False)
-    category: Mapped
+    category_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey("categories.id", ondelete="set null"))
+    category: Mapped["Category"] = relationship(back_populates="products")
 
 
 class Category(Base):
-    __tablename__ = "category"
+    __tablename__ = "categories"
     name: Mapped[str] = mapped_column(String(255))
+    products: Mapped[list["Product"]] = relationship(back_populates="category")
