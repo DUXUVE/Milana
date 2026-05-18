b7614f5d2812c203a19dc252ac2fae06657a5470
Milana <m.milana2007.mil@gmail.com>
2026-05-18 21:30:47 +0300

    Add files via upload

---
+++ !cw05-13js/script.js
@@ -0,0 +1,15 @@
+import { Notification, NotificationType } from "./Notifier/notification.js";
+import { Notifier } from "./Notifier/notifier.js";
+
+const element = document.createElement('div');
+
+const notification = new Notification(element, NotificationType.WARNING,"123123123123123");
+notification.initElement();
+document.body.appendChild(element);
+
+const notifier = Notifier.createAttachedElement();
+
+notifier.showWarning("рфпфп");
+notifier.showError("error");
+notifier.showInfo("dsgsgd");
+notifier.showSuccess("sdgsgcd");
