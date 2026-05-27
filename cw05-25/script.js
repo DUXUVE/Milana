50cabde9f95f57c66b33c1b1959c3d61c043255e
kaz1nak666 <impreezzza@gmail.com>
2026-05-27 21:32:40 +0300

    22

--- cw05-25/script.js
+++ cw05-25/script.js
@@ -1,3 +1,3 @@
 import { Carousel } from "./Carousel/carousel.js";
 
-new Carousel(document.querySelector(".carousel"), document.querySelector(".images"));
+new Carousel(document.querySelector(".carousel"), [...document.querySelectorAll(".images")]);
