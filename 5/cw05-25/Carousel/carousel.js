fdc16257cc0fc1b9e0a107f94cbc63115ba1ff4b
DUXUVE <m.milana2007.mil@gmail.com>
2026-06-01 21:36:44 +0300

    123456

--- 5/cw05-25/Carousel/carousel.js
+++ 5/cw05-25/Carousel/carousel.js
@@ -1,12 +1,12 @@
 // блок внутри которого будет хранится массив с элементами 
 // которые можно по клику на правую и левую стрелочку менять
 
-export class Carousel {
+export class Carousel { 
     /**
      * @param {HTMLElement} element
      * @param {Array<HTMLElement>} childrenElement
      */
-    constructor(element, childrenElement) {
+    constructor(element, childrenElement, switchTimeout=null) {
         // Основной элемент-контейнер, в котором то что перематывать и кнопочки
         this._element = element;
         this._childElements = childrenElement;
@@ -21,11 +21,24 @@ export class Carousel {
         const carouselInner = document.querySelector(".carousel-inner");
         this._index = 0;
         this._leftBtn.addEventListener('click', () => {
+            if (this._setInterval) { clearInterval(this._setInterval); }
             this._index = this._index === 0 ? this._childElements.length - 1 : this._index - 1;
+            carouselInner.style.left = `-${this._index * 100}%`;    
+        });
+        this._rightBtn.addEventListener('click', () => {
+            if (this._setInterval) { clearInterval(this._setInterval); }
+            this._index = this._index === this._childElements.length - 1 ? 0 : this._index + 1;
             carouselInner.style.left = `-${this._index * 100}%`;     
         });
 
         this._element.append(this._leftBtn, this._rightBtn);
+
+        if (switchTimeout != null) {
+            this._setInterval = setInterval(() => {
+                this._index = this._index === 0 ? this._childElements.length - 1 : this._index - 1;
+                carouselInner.style.left = `-${this._index * 100}%`;
+            }, switchTimeout);
+        } 
     }
 
     
--- 5/cw05-25/script.js
+++ 5/cw05-25/script.js
@@ -1,3 +1,3 @@
 import { Carousel } from "./Carousel/carousel.js";
 
-new Carousel(document.querySelector(".carousel"), [...document.querySelectorAll(".images")]);
+new Carousel(document.querySelector(".carousel"), [...document.querySelectorAll(".images")], 1000);
