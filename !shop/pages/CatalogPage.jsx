daa0a9baaca28a1ea465f0f0b21c128be3ed1755
DUXUVE <m.milana2007.mil@gmail.com>
2026-06-28 17:56:34 +0300

    124325

--- !shop/pages/CatalogPage.jsx
+++ !shop/pages/CatalogPage.jsx
@@ -1,5 +1,15 @@
+import products from "./products"
+
 function CatalogPage() {
-    return <div>Товары</div>
+    return <div>
+        Товары
+        <ul>{products.map((product) => (
+            <li><article>
+                {product.title}
+            </article></li>
+        ))}
+        </ul>
+        </div>
 }
 
 export default CatalogPage
--- !shop/src/RootLayout.jsx
+++ !shop/src/RootLayout.jsx
@@ -1,4 +1,5 @@
 import { useRouteLoaderData } from "react-router"
+import "./RootLayout.css"
 
 export function RootLayout() {
 
@@ -8,10 +9,20 @@ export function RootLayout() {
                 <link className="brand" to="/">
                     TreadLine
                 </link>
+                <nav className="nav-links" aria-label="Primary navigation">
+                    <NavLink className = { (o) => `${o.isPending ? "pending": ""} ${o.isActive ? "active": ""}` } to="/">
+                        Каталог
+                    </NavLink>
+                    <NavLink className = { (o) => `${o.isPending ? "pending": ""} ${o.isActive ? "active": ""}` } to="/about">
+                        О компании
+                    </NavLink>
+                </nav>
             </header>
             <main> 
                 <Outlet />
             </main>
         </div>
     )
-}
+} 
+
+export default RootLayout
